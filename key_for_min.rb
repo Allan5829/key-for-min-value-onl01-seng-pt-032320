013d48dd8883f1940c5e099e884ed53e0163b289
Allan Farca <allan.farca@gmail.com>
2020-04-04 04:46:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,8 @@ def key_for_min_value(name_hash)
   min_value = 0
   name_hash.each do |key, value|
     if (value < min_value)
-      
+      min_value = value
+      return_key = key
     end
   end
   return_key
