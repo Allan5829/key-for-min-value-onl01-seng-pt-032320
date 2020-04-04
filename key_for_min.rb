199e0e837ef28e8b1e7e26a3bee41763e39c5a7a
Allan Farca <allan.farca@gmail.com>
2020-04-04 04:38:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,15 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  true_array = []
   name_hash.each do |key, value|
-    value.all? do |number|
-      true_array << number <= value [0]
-      true_array << number <= value [1]
-      true_array << number <= value [2]
-    end
-    if (true_array == true)
-      return key
-    end
   end
 end
