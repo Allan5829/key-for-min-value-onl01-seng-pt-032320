c0fd0b865f576c280f99d6c3dad0768fcb2f1eed
Allan Farca <allan.farca@gmail.com>
2020-04-04 04:41:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  return_key = NIL
+  return_key = nil
   name_hash.each do |key, value|
   end
+  return_key
 end
