e88a9c4579e0997573f1627251746ae9ff581240
Allan Farca <allan.farca@gmail.com>
2020-04-04 04:16:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  true_array
   name_hash.each do |key, value|
+    
+    
+    
+    
+    
+    
+    
   end
 end
