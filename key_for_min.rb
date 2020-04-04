a2ac476e4e51e90cd9b51977df37a7f22b22e1a9
Allan Farca <allan.farca@gmail.com>
2020-04-04 04:28:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
   true_array = []
   name_hash.each do |key, value|
     value.all? do |number|
-      number <= value [0]
-      number <= value [1]
-      number <= value [2]
+      true_array << number <= value [0]
+      true_array << number <= value [1]
+      true_array << number <= value [2]
     end
     if (true_array == true)
       return key
